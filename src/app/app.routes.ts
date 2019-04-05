21623d3599b324f3295a6383e1fd03118158a405
Samuel Marks <samuelmarks@gmail.com>
2019-04-05 22:31:20 +1100

    [package.json,package-lock.json,server-status.component.ts] Bumped version; [dashboard] Removed demo dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,17 @@ import { AuthGuard } from './auth/auth.guard';
 
 
 export const appRoutes: Routes = [
-  { path: '', loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
   {
     path: 'admin', loadChildren: './admin/admin.module#AdminModule',
     canActivate: [AuthGuard], data: { role: ['admin'] }
   },
   {
-    path: 'secret-dashboard', loadChildren: './secret-dashboard/secret-dashboard.module#SecretDashboardModule',
+    path: '', loadChildren: './secret-dashboard/secret-dashboard.module#SecretDashboardModule',
     canActivate: [AuthGuard]
   },
   {
-    path: 'secret-dashboard/analytics', loadChildren: './analytics/analytics.module#AnalyticsModule',
+    path: 'analytics', loadChildren: './analytics/analytics.module#AnalyticsModule',
     canActivate: [AuthGuard]
   },
 ];
