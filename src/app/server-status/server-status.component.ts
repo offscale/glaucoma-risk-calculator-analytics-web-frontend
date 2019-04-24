9f68caec30839e3307fd1e9e71e14d36946bf9b1
Samuel Marks <samuelmarks@gmail.com>
2019-04-24 16:10:12 +1000

    [package.json,package-lock.json] Bumped version; upgraded dependencies;

--- src/app/server-status/server-status.component.ts
+++ src/app/server-status/server-status.component.ts
@@ -18,7 +18,7 @@ export class ServerStatusComponent implements OnInit {
               private alertsService: AlertsService) {}
 
   ngOnInit() {
-    this.serverStatus = { version: '@ 0.0.7; '};
+    this.serverStatus = { version: '@ 0.0.8; '};
     this.serverStatusService
       .get()
       .subscribe(
