fd1e2755e7ac2e3cfd18ead186424d1ef270fda1
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-08-09 10:58:56 +1000

    [package.json,package-lock.json] Upgraded dependencies; bump version

--- src/app/server-status/server-status.component.ts
+++ src/app/server-status/server-status.component.ts
@@ -21,7 +21,7 @@ export class ServerStatusComponent implements OnInit {
               private alertsService: AlertsService) {}
 
   ngOnInit() {
-    this.serverStatus = { version: '@ 0.0.10; ' };
+    this.serverStatus = { version: '@ 0.0.11; ' };
     forkJoin([
       this.serverStatusService
         .get(),
